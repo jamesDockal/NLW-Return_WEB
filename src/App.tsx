dc49d50aedc4b3975ab50227c1896b694601368d
James <jamesistranho@gmail.com>
2022-05-02 23:38:43 -0300

    create Widget component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { Widget } from "./components/Widget";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-     <button className='button' >teste</button>
+      <Widget />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
