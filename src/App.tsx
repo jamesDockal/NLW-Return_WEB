b2c645c7ecac5a93a54fdab64de515b0c06436b8
James <jamesistranho@gmail.com>
2022-05-02 23:10:41 -0300

    base project structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <button className='button' >teste</button>
+    </div>
+  )
+}
+
+export default App
